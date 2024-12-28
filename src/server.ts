daa27e9a41a9b899fc013a5870dee6b03947c48f
linzero <root@linli.eu.org>
2024-12-28 15:48:34 +0800

    21

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import { expressjwt as jwt } from 'express-jwt';
 import { DataSource } from 'typeorm';
